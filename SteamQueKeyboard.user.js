fbfd3e5879b16f0d7342f3aa6592778d64581c55
Eliott <bryant.eliott@gmail.com>
2022-12-26 23:40:33 -0500

    update to work better, and make the que seamless
    will probably need to come clean this later

--- SteamQueKeyboard.user.js
+++ SteamQueKeyboard.user.js
@@ -1,95 +1,127 @@
 // ==UserScript==
-// @name       Steam que via keyboard.
-// @namespace  https://github.com/Plaer1/SteamQueKeyboardShortcuts/
-// @version    0.3.3
-// @description  lets you navigate you steam queue via keybaord, specifically the arrow keys
-// @match      https://store.steampowered.com/*
-// @copyright  Gnu GPL V2
+// @name         Steam que via keyboard.
+// @namespace    https://github.com/Plaer1/SteamQueKeyboardShortcuts/
+// @version      0.3.3.3
+// @description  lets you navigate you steam queue via the arrow keys
+// @match        https://store.steampowered.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=steampowered.com
+// @copyright    Gnu GPL V2
+// @require      https://code.jquery.com/jquery-3.6.3.min.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js
+// @grant        none
 // ==/UserScript==
 
-(function(){
-document.addEventListener('keydown', function(e) {
-    'use strict';
-    switch (e.keyCode) {
 
-        //left
-        case 37:
+(function ($, undefined) {
 
-            if(document.querySelector("#add_to_wishlist_area").style.display == "none") {
-                document.querySelector("#add_to_wishlist_area_success > a > span").click();
-            }
+	(function(){
+		document.addEventListener('keydown', function(e) {
 
-            else {
-                document.querySelector("#add_to_wishlist_area > a > span").click();
-            }
+			'use strict';
 
-            break;
+			switch (e.keyCode) {
 
-        //up
-        case 38:
+				//left
+				case 37:
 
-                if (document.querySelector("#queueBtnFollow > div.btnv6_blue_hoverfade.btn_medium.queue_btn_inactive").style.display != "none") {
-                     // click to follow
-                    document.querySelector("#queueBtnFollow > div.btnv6_blue_hoverfade.btn_medium.queue_btn_inactive").click();
-                } else {
-                    //Click to UNfollow
-                    document.querySelector("#queueBtnFollow > div.btnv6_blue_hoverfade.btn_medium.queue_btn_active").click();
-                }
+					if(document.querySelector("#add_to_wishlist_area").style.display == "none") {
+						document.querySelector("#add_to_wishlist_area_success > a > span").click();
+					}
+					else {
+						document.querySelector("#add_to_wishlist_area > a > span").click();
+					}
 
-            break;
+					break;
 
-        //right
-        case 39:
-            //if in que go next
-            if (document.querySelector("#nextInDiscoveryQueue > div.btn_next_in_queue.btn_next_in_queue_trigger") != null){
-                document.querySelector("#nextInDiscoveryQueue > div.btn_next_in_queue.btn_next_in_queue_trigger").click();
-                break;
-            }
-            //if the que needs re-started
-            if (document.querySelector("#refresh_queue_btn") != null){
-                document.querySelector("#refresh_queue_btn").click();
-                break;
-            }
-            //if the que needs started, resume it bb
-            if (document.querySelector("#discovery_queue_start_link") != null){
-                document.querySelector("#discovery_queue_start_link").click();
-                break;
-            }
-            //if you aren't in the que, hop in bb.
-            if (document.querySelector("#queueActionsCtn > a") != null){
-                document.querySelector("#queueActionsCtn > a").click();
-                break;
-            }
-            //if all else fails refresh the damn page
-            location.reload();
-            break;
+				//up
+				case 38:
 
-        //down
-        case 40:
-            //if a game
-            if (document.querySelector("#game_area_purchase > div.game_area_bubble.game_area_dlc_bubble") == null) {
-                if (document.querySelector("#ignoreBtn > div.queue_control_button.queue_btn_ignore > div.btnv6_blue_hoverfade.btn_medium.queue_btn_inactive").style.display != "none") {
-                    document.querySelector("#ignoreBtn > div.queue_control_button.queue_btn_ignore > div.btnv6_blue_hoverfade.btn_medium.queue_btn_inactive").click();
-                }
-                else {
-                    document.querySelector("#ignoreBtn > div.queue_control_button.queue_btn_ignore > div.btnv6_blue_hoverfade.btn_medium.queue_btn_active").click();
-                }
-            }
-            //if DLC
-            if (document.querySelector("#game_area_purchase > div.game_area_bubble.game_area_dlc_bubble") != null) {
-                if (document.querySelector("#ignoreBtn > div.queue_control_button.queue_btn_ignore > div.btnv6_blue_hoverfade.btn_medium.queue_btn_inactive").style.display != "none") {
-                    document.querySelector("#ignoreBtn > div.queue_control_button.queue_btn_ignore > div.btnv6_blue_hoverfade.btn_medium.queue_btn_inactive").click();
-                }
-                else {
-                    document.querySelector("#ignoreBtn > div.queue_control_button.queue_btn_ignore > div.btnv6_blue_hoverfade.btn_medium.queue_btn_active").click();
-                }
-            }
-            break;
+					if (document.querySelector("#queueBtnFollow > div.btnv6_blue_hoverfade.btn_medium.queue_btn_inactive").style.display != "none") {
+						// click to follow
+						document.querySelector("#queueBtnFollow > div.btnv6_blue_hoverfade.btn_medium.queue_btn_inactive").click();
+					} else {
+						//Click to UNfollow
+						document.querySelector("#queueBtnFollow > div.btnv6_blue_hoverfade.btn_medium.queue_btn_active").click();
+					}
 
-        default:
-            //quit
-            break;
+					break;
 
-    }
-}, false);
-})();
+				//right
+				case 39:
+
+					//if in que, go next
+					if (document.querySelector(".queue_sub_text") != null){
+						document.querySelector(".btn_next_in_queue").click();
+						break;
+					}
+					//if on a game page that isn't in the que, start que.
+					if (document.querySelector("[data-tooltip-text='View and customize your personal Discovery Queue.']") != null){
+						window.location.href = "https://store.steampowered.com/explore/next/0?snr=1_5_9__12";
+						break;
+					}
+					//if at the end, restart que
+					if (document.querySelector(".finish_queue_text") != null) {
+						$.post( "https://store.steampowered.com/explore/next/discovery/",
+						{
+							"data": $("#next_in_queue_form").serialize(),
+							"success": () => { window.location.href = `https://store.steampowered.com/explore/startnew/discovery/`
+						},
+						});
+						break;
+					}
+					//if at que page with empty que, restart que (this often doesn't work)
+					if (document.querySelector(".keepGoing") != null) {
+						$.post( "https://store.steampowered.com/explore/next/discovery/",
+						{
+							"data": $("#next_in_queue_form").serialize(),
+							"success": () => { window.location.href = `https://store.steampowered.com/explore/startnew/discovery/`
+						},
+						});
+						break;
+					}
+					//if you aren't in the que, hop in bb.
+					//if (document.querySelector(".discovery_queue_start_link") != null){
+					//	document.querySelector(".discovery_queue_start_link").click();
+					//	break;
+					//}
+
+					break;
+
+				//down
+				case 40:
+					//if a game
+					if (document.querySelector("#game_area_purchase > div.game_area_bubble.game_area_dlc_bubble") == null) {
+						if (document.querySelector("#ignoreBtn > div.queue_control_button.queue_btn_ignore > div.btnv6_blue_hoverfade.btn_medium.queue_btn_inactive").style.display != "none") {
+							document.querySelector("#ignoreBtn > div.queue_control_button.queue_btn_ignore > div.btnv6_blue_hoverfade.btn_medium.queue_btn_inactive").click();
+						}
+						else {
+							document.querySelector("#ignoreBtn > div.queue_control_button.queue_btn_ignore > div.btnv6_blue_hoverfade.btn_medium.queue_btn_active").click();
+						}
+					}
+					//if DLC
+					if (document.querySelector("#game_area_purchase > div.game_area_bubble.game_area_dlc_bubble") != null) {
+						if (document.querySelector("#ignoreBtn > div.queue_control_button.queue_btn_ignore > div.btnv6_blue_hoverfade.btn_medium.queue_btn_inactive").style.display != "none") {
+							document.querySelector("#ignoreBtn > div.queue_control_button.queue_btn_ignore > div.btnv6_blue_hoverfade.btn_medium.queue_btn_inactive").click();
+						}
+						else {
+							document.querySelector("#ignoreBtn > div.queue_control_button.queue_btn_ignore > div.btnv6_blue_hoverfade.btn_medium.queue_btn_active").click();
+						}
+					}
+					break;
+
+				default:
+					//quit
+					break;
+
+			}
+		}, false);
+	})();
+	// refreshes on que if you end up on the page between refreshes somehow
+	if (document.querySelector(".discovery_queue_static").style.display == "block"){
+		document.querySelector(".discovery_queue_overlay").click();
+	}
+	// refreshes on que "error"
+	if (document.querySelector(".error") != null){
+		location.reload();
+	}
+})(window.jQuery.noConflict(true));
